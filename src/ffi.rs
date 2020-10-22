cf4f14904e772fb222b4ea9108a23f6467340d58
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2020-10-22 12:14:18 +0900

    RIIR: colour.c

--- src/ffi.rs
+++ src/ffi.rs
@@ -4,3 +4,12 @@
 #![allow(dead_code)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+macro_rules! c_try {
+    ($e:expr, $ret:expr) => {
+        match $e {
+            Ok(s) => s,
+            Err(_) => return $ret,
+        }
+    };
+}
