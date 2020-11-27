993526b2228147b468808bdd4fea91e550d6d549
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2020-11-27 12:23:00 +0900

    Re-re-organize module structure

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,162 @@
     unused_assignments,
     unused_mut
 )]
-#![register_tool(c2rust)]
 #![feature(
+    c_variadic,
     const_raw_ptr_to_usize_cast,
+    const_transmute,
     extern_types,
+    label_break_value,
     main,
     ptr_wrapping_offset_from,
     register_tool
 )]
+#![register_tool(c2rust)]
 
-use c2rust_out::{
-    environ::environ,
-    options::{options, options_entry},
-};
+#[macro_use]
+extern crate c2rust_bitfields;
+extern crate libc;
+
+pub mod alerts;
+pub mod arguments;
+pub mod attributes;
+pub mod cfg;
+pub mod client;
+pub mod cmd;
+pub mod cmd_attach_session;
+pub mod cmd_bind_key;
+pub mod cmd_break_pane;
+pub mod cmd_capture_pane;
+pub mod cmd_choose_tree;
+pub mod cmd_command_prompt;
+pub mod cmd_confirm_before;
+pub mod cmd_copy_mode;
+pub mod cmd_detach_client;
+pub mod cmd_display_menu;
+pub mod cmd_display_message;
+pub mod cmd_display_panes;
+pub mod cmd_find;
+pub mod cmd_find_window;
+pub mod cmd_if_shell;
+pub mod cmd_join_pane;
+pub mod cmd_kill_pane;
+pub mod cmd_kill_server;
+pub mod cmd_kill_session;
+pub mod cmd_kill_window;
+pub mod cmd_list_buffers;
+pub mod cmd_list_clients;
+pub mod cmd_list_keys;
+pub mod cmd_list_panes;
+pub mod cmd_list_sessions;
+pub mod cmd_list_windows;
+pub mod cmd_load_buffer;
+pub mod cmd_lock_server;
+pub mod cmd_move_window;
+pub mod cmd_new_session;
+pub mod cmd_new_window;
+pub mod cmd_parse;
+pub mod cmd_paste_buffer;
+pub mod cmd_pipe_pane;
+pub mod cmd_queue;
+pub mod cmd_refresh_client;
+pub mod cmd_rename_session;
+pub mod cmd_rename_window;
+pub mod cmd_resize_pane;
+pub mod cmd_resize_window;
+pub mod cmd_respawn_pane;
+pub mod cmd_respawn_window;
+pub mod cmd_rotate_window;
+pub mod cmd_run_shell;
+pub mod cmd_save_buffer;
+pub mod cmd_select_layout;
+pub mod cmd_select_pane;
+pub mod cmd_select_window;
+pub mod cmd_send_keys;
+pub mod cmd_set_buffer;
+pub mod cmd_set_environment;
+pub mod cmd_set_option;
+pub mod cmd_show_environment;
+pub mod cmd_show_messages;
+pub mod cmd_show_options;
+pub mod cmd_source_file;
+pub mod cmd_split_window;
+pub mod cmd_swap_pane;
+pub mod cmd_swap_window;
+pub mod cmd_switch_client;
+pub mod cmd_unbind_key;
+pub mod cmd_wait_for;
+pub mod colour;
+pub mod compat {
+    pub mod closefrom;
+    pub mod fdforkpty;
+    pub mod fgetln;
+    pub mod freezero;
+    pub mod getdtablecount;
+    pub mod getopt;
+    pub mod getprogname;
+    pub mod imsg;
+    pub mod imsg_buffer;
+    pub mod recallocarray;
+    pub mod setproctitle;
+    pub mod strlcat;
+    pub mod strlcpy;
+    pub mod strtonum;
+    pub mod unvis;
+    pub mod vis;
+} // mod compat
+pub mod control;
+pub mod control_notify;
+pub mod environ;
+pub mod file;
+pub mod format;
+pub mod format_draw;
+pub mod grid;
+pub mod grid_view;
+pub mod input;
+pub mod input_keys;
+pub mod job;
+pub mod key_bindings;
+pub mod key_string;
+pub mod layout;
+pub mod layout_custom;
+pub mod layout_set;
+pub mod log;
+pub mod menu;
+pub mod mode_tree;
+pub mod names;
+pub mod notify;
+pub mod options;
+pub mod options_table;
+pub mod osdep_linux;
+pub mod paste;
+pub mod popup;
+pub mod r#proc;
+pub mod regsub;
+pub mod resize;
+pub mod screen;
+pub mod screen_redraw;
+pub mod screen_write;
+pub mod server;
+pub mod server_client;
+pub mod server_fn;
+pub mod session;
+pub mod spawn;
+pub mod status;
+pub mod style;
+pub mod tty;
+pub mod tty_acs;
+pub mod tty_features;
+pub mod tty_keys;
+pub mod tty_term;
+pub mod utf8;
+pub mod window;
+pub mod window_buffer;
+pub mod window_client;
+pub mod window_clock;
+pub mod window_copy;
+pub mod window_customize;
+pub mod window_tree;
+pub mod xmalloc;
 
 extern "C" {
     pub type event_base;
@@ -97,39 +240,42 @@ extern "C" {
     #[no_mangle]
     fn set_cfg_file(_: *const libc::c_char);
     #[no_mangle]
-    fn options_create(_: *mut options) -> *mut options;
+    fn options_create(_: *mut crate::options::options) -> *mut crate::options::options;
     #[no_mangle]
-    fn options_default(_: *mut options, _: *const options_table_entry) -> *mut options_entry;
+    fn options_default(
+        _: *mut crate::options::options,
+        _: *const options_table_entry,
+    ) -> *mut crate::options::options_entry;
     #[no_mangle]
     fn options_set_string(
-        _: *mut options,
+        _: *mut crate::options::options,
         _: *const libc::c_char,
         _: libc::c_int,
         _: *const libc::c_char,
         _: ...
-    ) -> *mut options_entry;
+    ) -> *mut crate::options::options_entry;
     #[no_mangle]
     fn options_set_number(
-        _: *mut options,
+        _: *mut crate::options::options,
         _: *const libc::c_char,
         _: libc::c_longlong,
-    ) -> *mut options_entry;
+    ) -> *mut crate::options::options_entry;
     #[no_mangle]
     static options_table: [options_table_entry; 0];
     #[no_mangle]
-    fn environ_create() -> *mut environ;
+    fn environ_create() -> *mut crate::environ::environ;
     #[no_mangle]
-    fn environ_find(_: *mut environ, _: *const libc::c_char) -> *mut environ_entry;
+    fn environ_find(_: *mut crate::environ::environ, _: *const libc::c_char) -> *mut environ_entry;
     #[no_mangle]
     fn environ_set(
-        _: *mut environ,
+        _: *mut crate::environ::environ,
         _: *const libc::c_char,
         _: libc::c_int,
         _: *const libc::c_char,
         _: ...
     );
     #[no_mangle]
-    fn environ_put(_: *mut environ, _: *const libc::c_char, _: libc::c_int);
+    fn environ_put(_: *mut crate::environ::environ, _: *const libc::c_char, _: libc::c_int);
     #[no_mangle]
     fn tty_add_features(_: *mut libc::c_int, _: *const libc::c_char, _: *const libc::c_char);
     #[no_mangle]
@@ -733,16 +879,20 @@ unsafe extern "C" fn lstat(
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #[no_mangle]
-pub static mut global_options: *mut options = 0 as *const options as *mut options;
+pub static mut global_options: *mut crate::options::options =
+    0 as *const crate::options::options as *mut crate::options::options;
 /* server options */
 #[no_mangle]
-pub static mut global_s_options: *mut options = 0 as *const options as *mut options;
+pub static mut global_s_options: *mut crate::options::options =
+    0 as *const crate::options::options as *mut crate::options::options;
 /* session options */
 #[no_mangle]
-pub static mut global_w_options: *mut options = 0 as *const options as *mut options;
+pub static mut global_w_options: *mut crate::options::options =
+    0 as *const crate::options::options as *mut crate::options::options;
 /* window options */
 #[no_mangle]
-pub static mut global_environ: *mut environ = 0 as *const environ as *mut environ;
+pub static mut global_environ: *mut crate::environ::environ =
+    0 as *const crate::environ::environ as *mut crate::environ::environ;
 #[no_mangle]
 pub static mut start_time: timeval = timeval {
     tv_sec: 0,
@@ -1312,9 +1462,9 @@ unsafe fn main_0(mut argc: libc::c_int, mut argv: *mut *mut libc::c_char) -> lib
             cwd,
         );
     }
-    global_options = options_create(0 as *mut options);
-    global_s_options = options_create(0 as *mut options);
-    global_w_options = options_create(0 as *mut options);
+    global_options = options_create(0 as *mut crate::options::options);
+    global_s_options = options_create(0 as *mut crate::options::options);
+    global_w_options = options_create(0 as *mut crate::options::options);
     oe = options_table.as_ptr();
     while !(*oe).name.is_null() {
         if (*oe).scope & 0x1 as libc::c_int != 0 {
